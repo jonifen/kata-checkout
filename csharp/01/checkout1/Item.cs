1f454e82064a08ab218e767fa218776ac9ab51e6
Jon Cain <jonifen@users.noreply.github.com>
2019-03-16 22:52:06 +0000

    commit an attempt in c#

---
+++ csharp/01/checkout1/Item.cs
@@ -0,0 +1,20 @@
+﻿namespace checkout1
+{
+    public class Item
+    {
+        public Item(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+
+        public Currency Price
+        {
+            get
+            {
+                return new Lists().Prices[Name];
+            }
+        }
+    }
+}
